11ca402f18e3a8af408f74507fc31c7ae268bb31
Alexander <alexander.rodrigo.calderon@gmail.com>
2022-06-24 06:51:36 -0700

    Initial Commit

---
+++ tecmusicMenu/tecmusicMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  tecmusicMenuApp.swift
+//  tecmusicMenu
+//
+//  Created by alexander rodrigo calderon eguiluz on 24/06/22.
+//
+
+import SwiftUI
+
+@main
+struct tecmusicMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
